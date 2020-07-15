3e3db2b7cfe17695df3ef5204ab880a4fd3001b4
unknown <pecheritsa.vv@gmail.com>
2020-07-15 15:19:35 +0300

    Init

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 const app = express();
 
 var corsOptions = {
-  origin: "http://localhost:8081"
+    origin: "http://localhost:8081"
 };
 
 app.use(cors(corsOptions));
@@ -18,21 +18,21 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 const db = require("./app/models");
 db.mongoose
-  .connect(db.url, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  })
-  .then(() => {
-    console.log("Connected to the database!");
-  })
-  .catch(err => {
-    console.log("Cannot connect to the database!", err);
-    process.exit();
-  });
+    .connect(db.url, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => {
+        console.log("Connected to the database!");
+    })
+    .catch(err => {
+        console.log("Cannot connect to the database!", err);
+        process.exit();
+    });
 
 // simple route
 app.get("/", (req, res) => {
-  res.json({ message: "Welcome to bezkoder application." });
+    res.json({ message: "Welcome to  application." });
 });
 
 require("./app/routes/task.routes")(app);
@@ -40,5 +40,5 @@ require("./app/routes/task.routes")(app);
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}.`);
+    console.log(`Server is running on port ${PORT}.`);
 });
