948ecd5aaa4a3f8acc1254b75919a98c4ab3cd5f
unknown <pecheritsa.vv@gmail.com>
2020-07-16 11:48:05 +0300

    Fix before push

--- server.js
+++ server.js
@@ -9,11 +9,6 @@ var corsOptions = {
 };
 
 app.use(cors(corsOptions));
-// app.use(function(req, res, next) {
-//     res.header("Access-Control-Allow-Origin", "YOUR-DOMAIN.TLD"); // update to match the domain you will make the request from
-//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//     next();
-// });
 // parse requests of content-type - application/json
 app.use(bodyParser.json());
 
