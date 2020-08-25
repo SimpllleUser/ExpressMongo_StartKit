b1c1ea667b5ca15d890139ce85c76dff32ddf54f
SimpllleUser <pecheritsa.vv@gmail.com>
2020-08-25 07:18:35 +0300

    Add index config for model

--- app/models/index.js
+++ app/models/index.js
@@ -9,4 +9,9 @@ db.url = dbConfig.url;
 db.tasks = require("./task.model.js")(mongoose);
 db.calendar_events = require("./calendar.model.js")(mongoose);
 
+db.user = require("./user.model")
+db.user = require("./role.model")
+
+db.ROLE = ["user", "admin", "moderator"]
+
 module.exports = db;
