9598b137ddd342cb01359cb53e1b448e897c59ba
Loldf <pecheritsa.vv@gmail.com>
2020-10-12 15:08:09 +0300

    Comment FIX

--- app/routes/project.routes.js
+++ app/routes/project.routes.js
@@ -1,9 +1,19 @@
+const { authJwt } = require("../middlewares/index");
+
 module.exports = app => {
+    app.use(function(req, res, next) {
+        res.header(
+            "Access-Control-Allow-Headers",
+            "x-access-token, Origin, Content-Type, Accept"
+        );
+        next();
+    });
+
     const project = require("../controllers/project.controller")
     var router = require("express").Router()
-
+    // ! Add middle ware
     router.post('/', project.creaet)
-    router.get('/', project.findAll)
+    router.get('/',  project.findAll)
     router.get('/:id', project.findOne)
     router.get('/allData/:id', project.allData)
     router.put('/:id', project.update)
