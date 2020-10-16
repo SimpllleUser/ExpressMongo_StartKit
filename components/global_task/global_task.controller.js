ccf6d544d00b09a1a3cb82fb878622f41e9485e6
Loldf <pecheritsa.vv@gmail.com>
2020-10-16 11:17:13 +0300

    Add last fix in GlobalTask

--- components/global_task/global_task.controller.js
+++ components/global_task/global_task.controller.js
@@ -43,15 +43,14 @@ exports.findAll = async(req, res) => {
 
 exports.findOne = async(req, res) => {
     const id = req.params.id
-    return res.send(id)
     if (!id) {
         return res.status(400).send({ message: "Content can not be empty!" })
     }
     try {
         const global_task = await GlobalTask.findById(id)
-        let tasks = await Task.find({ '_id': { $in: global_task.tasks } })
-        tasks = JSON.parse(JSON.stringify(tasks).replace(/_id/gi, 'id')) // Замена _id на id
-        global_task.tasks = tasks
+            // let tasks = await Task.find({ '_id': { $in: global_task.tasks } })
+            // tasks = JSON.parse(JSON.stringify(tasks).replace(/_id/gi, 'id')) // Замена _id на id
+            // global_task.tasks = tasks
         res.send(global_task);
     } catch (err) {
         res.status(500).send.message({ message: "Error retrieving Task with id=" + id })
@@ -76,45 +75,41 @@ exports.update = async(req, res) => {
             res.status(500).send({ message: "Error updating GlobalTask with id=" + id })
         })
 }
+exports.delete = async(req, res) => {
 
-exports.addTask = async(req, res) => {
-
-    if (!req.body) { return res.status(404).send({ message: "Data to update can not be empty!" }) }
-
-    const { id, task_id } = req.body
-
-    try {
-        await GlobalTask.findByIdAndUpdate({ _id: id }, { $push: { tasks: task_id } }, { useFindAndModify: false })
-    } catch (err) {
-        res.status(500).send({ message: err.message || "Error with add task" });
+        const id = req.body
+        try {
+            const test = await GlobalTask.findOneAndRemove(id, { useFindAndModify: false })
+            res.send({ message: "Delete success", test });
+        } catch (err) {
+            console.log(err)
+            res.status(500).send({ message: err || " Can`t delete task by id=" + task_id })
+        }
     }
-}
+    // exports.addTask = async(req, res) => {
 
-exports.getTasks = async(req, res) => {
-    try {
-        const id = req.body.id
-        const globalTask = await GlobalTask.findById(id)
-        const tasks_Id = globalTask.tasks;
+//     if (!req.body) { return res.status(404).send({ message: "Data to update can not be empty!" }) }
 
-        const tasks = await Task.find({
-            '_id': { $in: tasks_Id }
-        });
-        res.send(tasks);
-    } catch (err) {
-        res.status(404).send({ message: err })
-    }
-}
+//     const { id, task_id } = req.body
 
-exports.deleteTask = async(req, res) => {
+//     try {
+//         await GlobalTask.findByIdAndUpdate({ _id: id }, { $push: { tasks: task_id } }, { useFindAndModify: false })
+//     } catch (err) {
+//         res.status(500).send({ message: err.message || "Error with add task" });
+//     }
+// }
 
-    const { id, task_id } = req.body
+// exports.getTasks = async(req, res) => {
+//     try {
+//         const id = req.body.id
+//         const globalTask = await GlobalTask.findById(id)
+//         const tasks_Id = globalTask.tasks;
 
-    try {
-        await GlobalTask.findByIdAndUpdate({ _id: id }, { $pull: { tasks: task_id } }, { useFindAndModify: false })
-        await Task.findByIdAndRemove(task_id, { useFindAndModify: false })
-            // TODO Добавить подобній функционал для task.controller
-        res.send({ message: "Delete success" });
-    } catch (err) {
-        res.status(500).send({ message: err || " Can`t delete task by id=" + task_id })
-    }
-}
+//         const tasks = await Task.find({
+//             '_id': { $in: tasks_Id }
+//         });
+//         res.send(tasks);
+//     } catch (err) {
+//         res.status(404).send({ message: err })
+//     }
+// }
--- components/global_task/global_task.routes.js
+++ components/global_task/global_task.routes.js
@@ -1,15 +1,15 @@
 module.exports = app => {
     const global_task = require("./global_task.controller")
     var router = require("express").Router()
-    router.get('/:id', global_task.findOne)
     router.post('/', global_task.create)
+    router.get('/:id', global_task.findOne)
+    router.put('/:id', global_task.update)
+    router.delete('/:id', global_task.delete)
     router.get('/all/:project_id', global_task.findAll)
-        // router.post('/', global_task.create)
         // router.post('/create-task', global_task.addTask)
         // router.delete('/:id', project.delete)
-    router.get('/tasks', global_task.getTasks)
-        // router.delete('/delete-task', global_task.deleteTask)
-    router.put('/:id', global_task.update)
+        // router.get('/tasks', global_task.getTasks)
+
 
     app.use("/api/global-task", router);
 }
