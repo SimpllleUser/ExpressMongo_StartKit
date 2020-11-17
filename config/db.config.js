2751b77268d4ca1ff762941a0454bfd7761e3f01
SimpllleUser <raybek11@gmail.com>
2020-11-17 16:48:51 +0300

    last fix

--- config/db.config.js
+++ config/db.config.js
@@ -1,4 +1,4 @@
 module.exports = {
     url: 'mongodb+srv://Vitalik:loldf11165540@mydb.xsl6p.mongodb.net/StartKitApp?retryWrites=true&w=majority'
-        // url: "mongodb://localhost:27017/StartKitApp"
+        // url: "mongodb://localhost:27017/StartKitApp 111"
 };
