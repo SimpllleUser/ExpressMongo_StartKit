60f0d5d8f3cfa23b407b48e0265b249b90f29afd
SimpllleUser <raybek11@gmail.com>
2020-11-18 18:06:18 +0300

    add local url to connetc DB

--- config/db.config.js
+++ config/db.config.js
@@ -1,3 +1,5 @@
 module.exports = {
     url: 'mongodb+srv://Vitalik:loldf11165540@mydb.xsl6p.mongodb.net/StartKitApp?retryWrites=true&w=majority'
+
+    // url: "mongodb://localhost:27017/StartKitApp"
 };
