65a02a76d67ccfcfd0a5e048a85e90abf14b1677
SimpllleUser <raybek11@gmail.com>
2020-12-15 22:37:36 +0300

    Add normal origin url

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 const app = express();
 
 var corsOptions = {
-    origin: "http://localhost:5000"
+    origin: "http://localhost:3000"
 };
 
 app.use(cors(corsOptions));
@@ -82,6 +82,6 @@ require('./components/global_task/global_task.routes')(app);
 
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
+app.listen(8080, () => {
     console.log(`Server is running on port ${PORT}.`);
 });
